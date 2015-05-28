66d06319ae4e42d539fa66bc13555fb2e36e9522
agusbauer <agustinbauer1@gmail.com>
2015-05-28 15:16:47 -0300

    reserve-details xml

--- BusesLep/app/src/main/java/com/tecpro/buseslep/MainActivity.java
+++ BusesLep/app/src/main/java/com/tecpro/buseslep/MainActivity.java
@@ -3,18 +3,18 @@ package com.tecpro.buseslep;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+
 import android.widget.BaseAdapter;
-import android.widget.EditText;
+
 import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
+
+import com.tecpro.buseslep.search_scheludes.SearchScheludes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,23 +30,8 @@ public class MainActivity extends ActionBarActivity implements OnItemClickListen
         adaptador = new AdaptatorLastSearch(this);
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adaptador);
-        EditText searchText = (EditText) findViewById(R.id.editTxtSearch);
-        listView.setOnItemClickListener(this);
-        searchText.addTextChangedListener(new TextWatcher() {
-
-            public void afterTextChanged(Editable s) {
-
-                // you can call or do what you want with your EditText here
 
 
-            }
-
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-        });
     }
 
 
@@ -77,5 +62,20 @@ public class MainActivity extends ActionBarActivity implements OnItemClickListen
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+    }
+
+    public void launchSearchSchedules(View v){
+        Intent i =  new Intent(this, SearchScheludes.class);
+        startActivity(i);
+    }
+
+    public void launchLogin(View v){
+        //Intent i =  new Intent(this, SearchScheludes.class);
+        //startActivity(i);
+    }
+
+    public void launchRegister(View v){
+        //Intent i =  new Intent(this, SearchScheludes.class);
+        //startActivity(i);
     }
 }
