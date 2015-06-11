2701c9f8ee8407734af2fc5083060b119a7f45d7
jjaimez <jaimezjacinto@gmail.com>
2015-06-11 19:31:24 -0300

    stylos

--- BusesLep/app/src/main/java/com/tecpro/buseslep/Login.java
+++ BusesLep/app/src/main/java/com/tecpro/buseslep/Login.java
@@ -4,8 +4,14 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -19,10 +25,70 @@ import com.tecpro.buseslep.utils.SecurePreferences;
  */
 public class Login extends Activity {
 
+    private DrawerLayout drawerLayout;
+    private ListView drawer;
+    private ActionBarDrawerToggle toggle;
+    private static final String[] opciones = {"Inicio"};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        loadMenuOptions();
+    }
+    private void loadMenuOptions(){
+        // Rescatamos el Action Bar y activamos el boton Home
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setIcon(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
+        // Declarar e inicializar componentes para el Navigation Drawer
+        drawer = (ListView) findViewById(R.id.options_login);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout_login);
+
+        // Declarar adapter y eventos al hacer click
+        drawer.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, opciones));
+
+        drawer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                // Toast.makeText(SearchScheludes.this, "Pulsado: " + opciones[arg2], Toast.LENGTH_SHORT).show();
+                switch (arg2){
+                    case 0://presione recargar ciudades
+                        Intent i = new Intent(Login.this, MainActivity.class);
+                        startActivity(i);
+                        break;
+                }
+
+                drawerLayout.closeDrawers();
+
+            }
+        });
+
+        // Sombra del panel Navigation Drawer
+        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        // Integracion boton oficial
+        toggle = new ActionBarDrawerToggle(
+                this, // Activity
+                drawerLayout, // Panel del Navigation Drawer
+                R.drawable.ic_navigation_drawer, // Icono que va a utilizar
+                R.string.options, // Descripcion al abrir el drawer
+                R.string.app_name // Descripcion al cerrar el drawer
+        ){
+            public void onDrawerClosed(View view) {
+                // Drawer cerrado
+                getActionBar().setTitle(getResources().getString(R.string.app_name));
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                // Drawer abierto
+                getActionBar().setTitle(R.string.options);
+                invalidateOptionsMenu();
+            }
+        };
+
+        drawerLayout.setDrawerListener(toggle);
     }
 
 
